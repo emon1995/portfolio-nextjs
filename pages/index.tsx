2d92d8078789fa16bce2c2df2d12423e18505c81
emon1995 <emonhossain995@gmail.com>
2021-09-21 15:55:22 +0600

    Navbar & data create complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import { GetServerSidePropsContext } from 'next';
 import React from 'react';
 
-const Index = () => {
+const Index = ({ services }) => {
   return (
     <div>
       <p>Bye Bye World</p>
@@ -9,3 +10,16 @@ const Index = () => {
 };
 
 export default Index;
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const res = await fetch('http://localhost:3000/api/services');
+  const data = await res.json();
+
+  return {
+    props: {
+      services: data.services,
+    },
+  };
+};
