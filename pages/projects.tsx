781fe188bc8bf0a6ce054aeddc24417b14e0889a
emon1995 <emonhossain995@gmail.com>
2021-09-22 14:27:12 +0600

    Project Create

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import ProjectCard from '../components/ProjectCard';
+import { projects } from '../data';
 
 const Project = () => {
-  return <div className='p-4'>Project page</div>;
+  return (
+    <div className='p-4'>
+      <nav>Navbar</nav>
+      <div className='relative grid grid-cols-12 gap-4 my-3'>
+        {projects.map((project) => (
+          <div
+            className='col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200'
+            key={project.name}>
+            <ProjectCard project={project} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Project;
